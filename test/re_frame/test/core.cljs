b29da103c5d130a111964f8d714a09675aa583a3
Antonin Hildebrand <antonin@hildebrand.cz>
2015-11-04 18:10:22 +0100

    test proper execution order of dispatches

--- test/re_frame/test/core.cljs
+++ test/re_frame/test/core.cljs
@@ -48,3 +48,42 @@
       (is (= @target 10))
       (swap! core/app-db inc)
       (is (= @target 11)))))
+
+(deftest ^:async check-expected-order
+  (testing "dispatches should be executed in original order (async)"
+    (reinitialize!)
+    (reset! core/app-db [])
+    (core/register-handler :conj (fn [db [_ val]] (conj db val)))
+    (core/register-handler :check (fn [db [_ expected]]
+                                    (is (= db expected))
+                                    (done)
+                                    db))
+    (core/dispatch [:conj :first])
+    (core/dispatch [:conj :second])
+    (core/dispatch [:conj :third])
+    (core/dispatch [:check [:first :second :third]])))
+
+(deftest ^:async check-side-effecting-dispatches-order
+  (testing "dispatches causing other dispatches should queue them as FIFO (async)"
+    (reinitialize!)
+    (reset! core/app-db [])
+    (core/register-handler :conj (fn [db [_ val & events]]
+                                   (doseq [event events]
+                                     (core/dispatch event))
+                                   (conj db val)))
+    (core/register-handler :check (fn [db [_ expected]]
+                                    (is (= db expected))
+                                    (done)
+                                    db))
+    (core/dispatch [:conj :first-0
+                    [:conj :first-1
+                     [:conj :first-2]]])
+    (core/dispatch [:conj :second-0
+                    [:conj :second-1
+                     [:conj :second-2]]])
+    (core/dispatch [:conj :third-0
+                    [:conj :third-1
+                     [:conj :third-2]
+                     [:check [:first-0 :second-0 :third-0
+                              :first-1 :second-1 :third-1
+                              :first-2 :second-2 :third-2]]]])))
